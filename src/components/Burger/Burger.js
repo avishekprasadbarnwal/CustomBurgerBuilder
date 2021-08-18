911c08fb691dc98ea9dc76a8eba153c9627c583a
Avishek <avishekprasadbarnwal@gamil.com>
2021-08-18 08:31:19 +0530

    Burger builder basic components

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,32 @@
+// This file will contain all jsx for the Burger
+import React from 'react';
+import classes from './Burger.module.css';
+import BurgerIngredient from './BurgerIngredients/BurgerIngredient';
+
+const Burger = (props) => {
+
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey}></BurgerIngredient>
+            })
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el);
+        },[]);
+
+    // console.log(transformedIngredients);
+    if(transformedIngredients.length === 0 ){
+        transformedIngredients = <p>Please start inserting ingredient</p>
+    }
+
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredient type="bread-top"></BurgerIngredient>
+            {transformedIngredients}
+            <BurgerIngredient type="bread-bottom"></BurgerIngredient>
+        </div>
+    )
+}
+
+export default Burger;
